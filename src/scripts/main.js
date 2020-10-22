d31d6a4c1950e89227587a60a52f94a929713dfa
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-10-22 16:37:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const docBody = document.querySelector('body');
+
+const login = docBody.getElementById('login');
+const password = docBody.getElementById('password');
+const button = docBody.getElementById('submit');
+
+function waitFor(element, eventName) {
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, event => {
+      resolve(`You clicked on ${event.target.id}`);
+    });
+  });
+}
+
+const promiseOne = waitFor(login, 'click');
+const promiseTwo = waitFor(password, 'click');
+const promiseThree = waitFor(button, 'click');
+
+promiseOne.then(result => console.log(result));
+promiseTwo.then(result => console.log(result));
+promiseThree.then(result => console.log(result));
