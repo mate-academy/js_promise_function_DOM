12c81d71d3f9eef1b9c3645466e10230c955a937
Dima Diachenko <zarg129@gmail.com>
2020-10-26 14:24:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+function waitFor(element, eventName) {
+  return new Promise(resolve => {
+    element.addEventListener(eventName, e => {
+      resolve(`You have clicked on ${e.target.id}`);
+    });
+  });
+}
+
+function printName(val) {
+  console.log('the val is', val);
+}
+
+const login = document.getElementById('login');
+const pass = document.getElementById('password');
+const submit = document.getElementById('submit');
+
+const promise1 = waitFor(login, 'click');
+const promise2 = waitFor(pass, 'click');
+const promise3 = waitFor(submit, 'click');
+
+Promise.race([promise1, promise2, promise3])
+  .then(printName);
