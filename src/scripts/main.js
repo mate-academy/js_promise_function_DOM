d3309ac23612d4d601737c7f5a3ec7062d8164df
Anton <office.sklyarov@gmail.com>
2020-11-03 17:47:56 +0200

    added promise function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+function waitFor(element, eventName) {
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} and promise resolved`);
+    });
+  });
+}
+
+const loginInput = document.querySelector('#login');
+const passInput = document.querySelector('#password');
+
+function print(value) {
+  // eslint-disable-next-line no-console
+  console.log(value);
+}
+
+const promise1 = waitFor(loginInput, 'click');
+const promise2 = waitFor(passInput, 'blur');
+
+promise1
+  .then(print);
+
+promise2
+  .then(print);
