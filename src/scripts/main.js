f459714736df687b0940a90b58e9a99787c4ead8
y.kostenko <y.kostenko@mate.academy>
2020-11-12 17:36:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write your code here
+function waitFor(element, eventName) {
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element:
+          ${element.nodeName}, id: ${element.id}.`
+      );
+    });
+  });
+}
+
+const print = (message) => {
+  const divMessage = document.createElement('div');
+
+  divMessage.className = 'message';
+  divMessage.textContent = message;
+  document.querySelector('form').append(divMessage);
+};
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+const button = document.getElementById('submit');
+const promiseClick1 = waitFor(login, 'click');
+const promiseClick2 = waitFor(password, 'click');
+const promiseClick3 = waitFor(button, 'click');
+const promiseInput1 = waitFor(login, 'input');
+const promiseInput2 = waitFor(password, 'input');
+const promiseBlur1 = waitFor(login, 'blur');
+const promiseBlur2 = waitFor(password, 'blur');
+const promiseBlur3 = waitFor(button, 'blur');
+
+promiseClick1
+  .then(result => print(result));
+
+promiseClick2
+  .then(result => print(result));
+
+promiseClick3
+  .then(result => print(result));
+
+promiseInput1
+  .then(result => print(result));
+
+promiseInput2
+  .then(result => print(result));
+
+promiseBlur1
+  .then(result => print(result));
+
+promiseBlur2
+  .then(result => print(result));
+
+promiseBlur3
+  .then(result => print(result));
