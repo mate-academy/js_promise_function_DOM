eab2f105b2d592e01f1b4c2380fd351b1ec0e17f
kshvetsova <katshvetsova@gmail.com>
2020-11-21 03:06:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+const submit = document.getElementById('submit');
+
+function waitFor(element, eventName) {
+  return new Promise(resolve => {
+    element.addEventListener(eventName, (e) => {
+      e.preventDefault();
+      resolve(`${eventName}: ${e.target.id}`);
+    });
+  });
+}
+// eslint-disable-next-line no-console
+waitFor(login, 'blur').then(result => console.log(result));
+// eslint-disable-next-line no-console
+waitFor(password, 'change').then(result => console.log(result));
+// eslint-disable-next-line no-console
+waitFor(submit, 'click').then(result => console.log(result));
