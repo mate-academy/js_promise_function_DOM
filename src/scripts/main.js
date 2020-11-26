7b78ff492309d44d2b7af729a8fe9859683cfec6
Taras Sidak <tarasidak1990@gmail.com>
2020-11-26 12:50:51 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const login = document.querySelector('#login');
+const password = document.querySelector('#password');
+const button = document.querySelector('#submit');
+
+function waitFor(element, eventName) {
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`It works, you clicked ${e.target.id}`);
+    });
+
+    setTimeout(() => {
+      reject(new Error('Time is out'));
+    }, 25000);
+  });
+}
+
+const promise1 = waitFor(login, 'focus');
+const promise2 = waitFor(password, 'focus');
+const promise3 = waitFor(button, 'click');
+
+promise1
+  .then((result) => {
+    // eslint-disable-next-line no-console
+    console.log(result);
+  }, (error) => {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  });
+
+promise2
+  .then((result) => {
+    // eslint-disable-next-line no-console
+    console.log(result);
+  }, (error) => {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  });
+
+promise3
+  .then((result) => {
+    // eslint-disable-next-line no-console
+    console.log(result);
+  }, (error) => {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  });
