cecacc0484c129b8b6754da6835d4c603921f9c7
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-30 20:54:16 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const element = document.querySelector('#login');
+const element2 = document.querySelector('#password');
+const element3 = document.getElementById('submit');
+let target;
+
+document.addEventListener('click', (e) => {
+  e.preventDefault();
+  target = e.target;
+
+  const promise = waitFor(element3, target);
+
+  promise
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+});
+
+function waitFor(elem, eventName) {
+  return new Promise((resolve, reject) => {
+    if (elem === eventName) {
+      return resolve('Sucessfull');
+    }
+    reject('Error');
+  });
+}
