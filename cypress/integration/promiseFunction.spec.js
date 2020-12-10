9e90019623046e58afcbf02a565d7a7c8891e25d
Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
2020-12-10 18:30:20 +0200

    Update promiseFunction.spec.js

--- cypress/integration/promiseFunction.spec.js
+++ cypress/integration/promiseFunction.spec.js
@@ -12,44 +12,44 @@ describe('Promise function app', () => {
 
   it(`should resolve 'click' promise for 'login' field`, () => {
     cy.get('#login').click();
-    cy.checkNotificationExist('click', 'INPUT', 'login');
+    cy.checkNotificationExists('click', 'INPUT', 'login');
   });
 
   it(`should resolve 'input' promise for 'login' field`, () => {
     cy.get('#login').type('login');
-    cy.checkNotificationExist('input', 'INPUT', 'login');
+    cy.checkNotificationExists('input', 'INPUT', 'login');
   });
 
   it(`should resolve 'blur' promise for 'login' field`, () => {
     cy.get('#login').click();
     cy.get('body').click({ force: true });
-    cy.checkNotificationExist('blur', 'INPUT', 'login');
+    cy.checkNotificationExists('blur', 'INPUT', 'login');
   });
 
   it(`should resolve 'click' promise for 'password' field`, () => {
     cy.get('#password').click();
-    cy.checkNotificationExist('click', 'INPUT', 'password');
+    cy.checkNotificationExists('click', 'INPUT', 'password');
   });
 
   it(`should resolve 'input' promise for 'password' field`, () => {
     cy.get('#password').type('login');
-    cy.checkNotificationExist('input', 'INPUT', 'password');
+    cy.checkNotificationExists('input', 'INPUT', 'password');
   });
 
   it(`should resolve 'blur' promise for 'password' field`, () => {
     cy.get('#password').click();
     cy.get('body').click({ force: true });
-    cy.checkNotificationExist('blur', 'INPUT', 'password');
+    cy.checkNotificationExists('blur', 'INPUT', 'password');
   });
 
   it(`should resolve 'click' promise for 'submit' button`, () => {
     cy.get('#submit').click();
-    cy.checkNotificationExist('click', 'BUTTON', 'submit');
+    cy.checkNotificationExists('click', 'BUTTON', 'submit');
   });
 
   it(`should resolve 'blur' promise for 'submit' button`, () => {
     cy.get('#submit').click();
     cy.get('body').click({ force: true });
-    cy.checkNotificationExist('blur', 'BUTTON', 'submit');
+    cy.checkNotificationExists('blur', 'BUTTON', 'submit');
   });
 });
