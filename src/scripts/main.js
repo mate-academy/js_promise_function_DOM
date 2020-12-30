915dfd95f356851d545ed40f630bf387f8a86766
losanov <losanov@ukr.net>
2020-12-30 14:57:26 +0200

    sol2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(`${eventName}`, (e) => {
+      const text = `It was ${eventName} on the element: ${
+        e.target.nodeName}, id: ${e.target.id}.`;
+
+      resolve(text);
+    }, true);
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const notification = document.createElement('p');
+  const div = document.querySelector('div');
+
+  div.style.position = 'fixed';
+  div.style.top = `10px`;
+  div.style.right = `10px`;
+  notification.className = 'message';
+  notification.textContent = message;
+  div.append(notification);
 };
 
 module.exports = {
