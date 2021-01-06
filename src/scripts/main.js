02daae5c02f88384685501b729a00906f02103ed
Inna Golovina <igigig978@gmail.com>
2021-01-06 14:13:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const newBlock = document.createElement('div');
+
+document.body.append(newBlock);
+
+newBlock.id = 'information';
+newBlock.style.top = '50px';
+newBlock.style.right = '50px';
+newBlock.style.position = 'absolute';
+
 function waitFor(element, eventName) {
-// write your code here
-}
+  return new Promise((resolve) => {
+    element.addEventListener(`${eventName}`, (e) => {
+      e.preventDefault();
+
+      const message = `It was ${eventName}`
+      + ` on the element: ${element.nodeName}, id: ${element.id}.`;
+
+      resolve(message);
+    }, true);
+  });
+};
 
 const printMessage = (message) => {
-// write your code here
+  const notification = document.createElement('div');
+
+  notification.classList.add(`message`);
+  notification.textContent = message;
+
+  document.getElementById('information').append(notification);
 };
 
 module.exports = {
