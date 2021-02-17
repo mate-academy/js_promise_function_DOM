277aca3a3191a8dc54ab18cadd283184483e64cb
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-02-17 21:43:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,41 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    switch (eventName) {
+      case 'click': element.addEventListener(('click'), () => {
+        resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
+      });
+
+        break;
+
+      case 'input': element.addEventListener(('click'), () => {
+        resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
+      });
+
+        break;
+
+      case 'blur': element.addEventListener(('click'), () => {
+        resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
+      });
+
+        break;
+    }
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+
+  div.classList.add('message');
+  p.innerText = message;
+
+  div.append(p);
+  document.body.append(div);
 };
 
 module.exports = {
