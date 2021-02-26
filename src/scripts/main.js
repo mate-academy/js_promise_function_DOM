a30380c98fe0e47bf60201467b37178921d00b98
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-02-26 08:59:06 +0200

    Add solution js_promise_function_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,21 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: 
+      ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
 // write your code here
+  const container = document.createElement('div');
+
+  container.className = 'message';
+  container.innerHTML = message;
+  document.body.append(container);
 };
 
 module.exports = {
