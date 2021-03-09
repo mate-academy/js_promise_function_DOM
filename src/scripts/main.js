1434476b4dda7c502590c765479b9f179568dd7e
Bohdan Dobak <chudolipton@gmail.com>
2021-03-09 17:47:39 +0200

    made function which return promise for special element and for special element event and create function printMessage which print notification about event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      if (eventName !== e.type) {
+        return;
+      }
+
+      resolve(
+        `It was ${eventName} on the element: `
+        + `${element.nodeName}, id: ${element.id}.`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const notification = document.createElement('div');
+
+  notification.textContent = message;
+  notification.classList.add('message');
+  document.body.append(notification);
 };
 
 module.exports = {
