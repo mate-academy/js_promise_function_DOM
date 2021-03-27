0a9598b44d68183ae1d5af3c01e98c6a515274f6
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-27 16:36:05 +0200

    promise function solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,28 @@
 'use strict';
 
+let countOfMessages = 0;
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(() => {
+    element.addEventListener(eventName, () => {
+      printMessage(
+        `It was ${eventName}
+        on the element: ${element.tagName},
+        id: ${element.id}`
+      );
+    }
+    );
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const textElement = document.createElement('div');
+
+  textElement.className = 'message';
+  textElement.innerText = message;
+  textElement.style.top = 10 + countOfMessages * 100 + 'px';
+  countOfMessages++;
+  document.body.append(textElement);
 };
 
 module.exports = {
