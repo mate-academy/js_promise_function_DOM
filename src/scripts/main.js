5528796bbf75b85c422d283a26273a995001195c
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-30 12:19:25 +0300

    Deploy check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element:`
+        + `${element.nodeName}, id: ${element.id}`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+
+  notification.textContent = message;
+  notification.classList.add('message');
+
+  body.append(notification);
 };
 
 module.exports = {
