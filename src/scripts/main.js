0b16cca4e88646e814cf56370fbad935cb044398
Julia <mjulia1794@gmail.com>
2021-04-14 15:18:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const form = document.querySelector('form');
+
 function waitFor(element, eventName) {
 // write your code here
+
+  const promise = new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+      ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
+
+  return promise;
 }
 
 const printMessage = (message) => {
 // write your code here
+  form.insertAdjacentHTML('beforeend',
+    `
+    <div class="message">${message}</div>
+    `
+  );
 };
 
 module.exports = {
