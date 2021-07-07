a76b219fef4644a364642e022769af034ffc4371
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-07 19:29:50 +0300

    feat: comleted task promise-function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the
+        element: ${element.nodeName}, id: ${element.id}.`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const createMessage = document.createElement('div');
+
+  createMessage.className = message;
+  createMessage.innerHTML = message;
+  document.body.appendChild(createMessage);
 };
 
 module.exports = {
