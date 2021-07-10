3f8932c4671c454ca20ba52942ba45b5c08d33a4
Bishko <yurik322412@gmail.com>
2021-07-10 14:21:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const messageContainer = document.querySelector('.message-container');
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve([eventName, element.nodeName, element.id]);
+    });
+  });
 }
 
-const printMessage = (message) => {
-// write your code here
+const printMessage = message => {
+  const [eventName, nodeName, elementId] = message;
+
+  messageContainer.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      It was ${eventName} on the element: ${nodeName}, id: ${elementId}.
+    </div>
+  `);
 };
 
 module.exports = {
