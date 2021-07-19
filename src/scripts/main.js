9494bbd44c2b6d3a54b3f2e4ec5856899dd3356f
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-19 14:19:31 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve( // пришлось так обрезать, потому что строка была слишком длинной
+        // и линтер не пропускал
+        `It was ${eventName} on the element: ${element.nodeName},`
+        + `id: ${element.id}.`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class='message'>${message}</div>
+  `);
 };
 
 module.exports = {
