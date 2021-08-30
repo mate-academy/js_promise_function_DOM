5bf435c27a70852e2466636baf36858d31573ed0
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-08-30 13:12:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,29 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  const msg = document.createElement('div');
+
+  msg.className = 'message';
+
+  msg.innerText = `It was ${eventName} on the element: `
+  + `${element.nodeName}, id: ${element.id}.`;
+
+  const resolver = (resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      if (e.target === element) {
+        resolve(msg);
+      }
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
 // write your code here
+  const rootElement = document.querySelector('body');
+
+  rootElement.append(message);
 };
 
 module.exports = {
