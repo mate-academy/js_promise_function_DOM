a172bd9543027c7b78452b4bf7684af2a55a60dd
Roman <romakelemen@gmail.com>
2021-09-09 15:37:33 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,33 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const resolver = (resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`It was ${eventName} on the element:
+       ${element.nodeName}, id: ${element.id}.`);
+    });
+  };
+
+  return new Promise(resolver);
 }
 
+const div = document.createElement('div');
+
+document.body.append(div);
+
 const printMessage = (message) => {
-// write your code here
+  const notification = document.createElement('div');
+
+  div.setAttribute('style',
+    `display: flex; 
+    flex-direction: column;
+    gap: 20px;
+    justify-content: space-evenly; 
+    flex-wrap: wrap;
+    align-items: center;`);
+  notification.classList.add('message');
+  notification.innerHTML = message;
+  div.append(notification);
 };
 
 module.exports = {
