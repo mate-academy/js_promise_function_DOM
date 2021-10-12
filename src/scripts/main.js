903883d39a8121c29b983f98572f1168bea63291
MaxStets <weawer458@gmail.com>
2021-10-12 15:52:57 +0300

    Solution -- promise_function_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,38 @@
 'use strict';
 
+const messagesBlock = `
+  <div 
+    class="messages"
+    style="
+      position: absolute; 
+      right: 0; top: 20%; 
+      display: flex; flex-direction: column;"
+    >
+  </div>
+`;
+
+document.body.insertAdjacentHTML('beforeend', messagesBlock);
+
+const nodeMessageBlock = document.querySelector('.messages')
+
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      const message = `It was ${eventName} on the element: `
+        + `${element.nodeName}, id: ${element.id}.`;
+
+      resolve(message);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const maskMessage = `
+    <div class="message" style="margin-bottom: 10px;">${message}</div>
+  `;
+
+  nodeMessageBlock.insertAdjacentHTML('beforeend', maskMessage);
 };
 
 module.exports = {
