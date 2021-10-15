05e50715d4b8a9a3c1b1add44863b507833ea686
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-10-15 13:47:07 +0300

    Task Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
+    }, { once: true });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+
+  notification.classList.add('message');
+  notification.innerHTML = message;
+  body.appendChild(notification);
 };
 
 module.exports = {
