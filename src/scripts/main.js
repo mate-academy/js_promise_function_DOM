413fd670016ac33abe75515794be1560ab6d91d5
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-10-16 13:15:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,49 @@
 'use strict';
 
+const initMessagesContainer = () => {
+  let container;
+
+  if (!document.getElementById('messages')) {
+    container = document.createElement('div');
+    container.id = 'messages';
+
+    const styles = {
+      position: 'absolute',
+      top: '20px',
+      right: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '10px',
+    };
+
+    for (const property in styles) {
+      container.style[property] = styles[property];
+    }
+
+    document.body.append(container);
+  }
+};
+
+initMessagesContainer();
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: \
+      ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const notification = `
+    <div class="message">
+      ${message}
+    </div>
+  `;
+
+  document.getElementById('messages')
+    .insertAdjacentHTML('beforeend', notification);
 };
 
 module.exports = {
