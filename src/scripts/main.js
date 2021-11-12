759b3c341e37f6f9b555a8231eb2fc688d7f1e1d
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-11-12 10:25:06 +0200

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const resolver = (resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: `
+      + `${element.nodeName}, id: ${element.id}.`);
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageElement = document.createElement('div');
+
+  messageElement.classList.add('message');
+  messageElement.innerHTML = message;
+  document.body.append(messageElement);
 };
 
 module.exports = {
