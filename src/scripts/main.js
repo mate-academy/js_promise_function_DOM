11151012e3a9e72129d593cb7a9c3e168acc5239
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-11-28 22:26:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName},
+       id: ${element.id}.`);
+    });
+  });
 }
 
+const notification = document.createElement('div');
+
 const printMessage = (message) => {
-// write your code here
+
+  notification.classList.add('message');
+  notification.innerText = message;
+
+  body.append(notification);
 };
 
 module.exports = {
