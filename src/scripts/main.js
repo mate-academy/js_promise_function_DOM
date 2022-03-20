2003def7dca001c21dc9750a55d85d9af4c1c7f5
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-20 21:09:05 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,31 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener('click', (e) => {
+      resolve(`${eventName} on the element: 
+      ${e.target.nodeName}, id: ${e.target.id}`);
+    });
+
+    element.addEventListener('contextmenu', (e) => {
+      resolve(`${eventName} on the element: 
+      ${e.target.nodeName}, id: ${e.target.id}`);
+    });
+
+    element.addEventListener('blur', (e) => {
+      resolve(`${eventName} on the element: 
+      ${e.target.nodeName}, id: ${e.target.id}`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.innerText = message;
+  body.append(div);
 };
 
 module.exports = {
