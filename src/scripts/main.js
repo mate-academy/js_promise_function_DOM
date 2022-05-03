19d80410cbc5b9fc353abd08a1f9e5b468ffa1e3
Dima Cherednyk <dicherednyk@gmail.com>
2022-05-03 21:04:19 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,29 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
-}
+  const resolver = (resolve, reject) => {
+    document.addEventListener(`${eventName}`, (e) => {
+      if (e.target === element) {
+        resolve(`It was
+        ${eventName} on the element: ${element.nodeName}, id: ${element.id}`
+        );
+      };
+    }, true);
+  };
+
+  return new Promise(resolver);
+};
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  body.lastChild.remove();
+  div.className = 'message';
+  div.textContent = message;
+  body.append(div);
+
+  waitFor().then(printMessage);
 };
 
 module.exports = {
