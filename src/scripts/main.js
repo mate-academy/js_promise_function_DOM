85176ecd85860eeaeea1cf00ae7fa84d1cc97bcf
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-05-08 18:54:23 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
-}
+  return new Promise((resolve, reject) => {
+    element.addEventListener(`${eventName}`, () => {
+      resolve(`
+      It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.
+      `);
+    });
+  });
+};
 
 const printMessage = (message) => {
-// write your code here
+  const alert = document.createElement('div');
+  
+  alert.classList.add('message');
+  alert.innerText = message;
+  document.body.appendChild(alert);
 };
 
 module.exports = {
