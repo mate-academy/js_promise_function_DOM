9865ff3adc490df3fc31ee2518ac0eed079560ed
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-16 17:35:37 +0300

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener('click', el => {
+      resolve(`It was ${eventName} on the element:
+      ${element.nodeName},
+      id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const element = document.createElement('div');
+
+  element.className = 'message';
+  element.textContent = message;
+  document.body.append(element);
 };
 
 module.exports = {
