56f89e582f6cb58db4ff34e6137ff74ba6b0d2f8
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-08-10 13:56:06 +0300

    solve the promiseFunctionDOM problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
+/* eslint-disable max-len */
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`
+        It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.
+      `);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${message}
+    </div>
+  `);
 };
 
 module.exports = {
