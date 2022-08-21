a107ee099412d3a386b5ba4cb2da3551968934c7
Olha Arama <arama_o_f@ukr.net>
2022-08-21 22:25:20 +0300

    Promise was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`
+        It was ${eventName} on the element: ${element.nodeName},
+        id: ${element.id}.
+      `);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  div.style.position = 'absolute';
+
+  document.body.append(div);
 };
 
 module.exports = {
