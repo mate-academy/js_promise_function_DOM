37edbc4d3d8f6a9910ffb862add040c7aede8277
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-15 15:34:43 +0300

    JS Promise Function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const resolver = (resolve, reject) => {
+    element.addEventListener(`${eventName}`, () => {
+      resolve(`It was ${eventName} on the element:
+        ${element.nodeName},
+        id: ${element.id}.`
+      );
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.innerHTML = message;
+  body.append(div);
 };
 
 module.exports = {
