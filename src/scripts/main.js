edb0803de3c4042a58eaefc5777086227b6dc0ed
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-09-15 12:54:19 +0300

    all done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,28 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
 // write your code here
+  const createMessage = document.createElement('div');
+
+  createMessage.className = 'message';
+  createMessage.innerText = message;
+
+  document.body.append(createMessage);
+
+  setTimeout(() => {
+    createMessage.remove();
+  }, 3000);
 };
 
 module.exports = {
