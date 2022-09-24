f8a5deda97ca479f568927a49015423466744cea
grafit-off <graf.off.it@gmail.com>
2022-09-24 17:35:14 +0300

    feat: promise waitFor func was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      const { nodeName, id } = element;
+
+      resolve(`It was ${eventName}
+      on the element: ${nodeName},
+      id: ${id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  // write your code here
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="message">${message}</div>`
+  );
 };
 
 module.exports = {
