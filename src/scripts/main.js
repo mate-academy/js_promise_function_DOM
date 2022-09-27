ceeb288673b39b6d94e8a9cea069076690404c98
Inna <dragotaiv@gmail.com>
2022-09-27 16:22:28 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
-// write your code here
-}
+  const promise = new Promise((resolve, reject) => {
+    body.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+       ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
+
+  return promise;
+};
 
 const printMessage = (message) => {
-// write your code here
+  body.insertAdjacentHTML('beforeend',
+    `<div class="message">${message}</div>`);
 };
 
 module.exports = {
