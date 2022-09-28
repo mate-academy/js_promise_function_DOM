fbf3cdf626fee91651e25ef2f7cfdc1234a74921
Tetiana Koval <tetiana.koval.bit@gmail.com>
2022-09-28 14:03:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`It was 
+      ${eventName} on the element: 
+      ${element.nodeName}, id: 
+      ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const newMessege = document.createElement('div');
+
+  newMessege.classList.add('message');
+  newMessege.innerText = message;
+  newMessege.style.position = 'absolute';
+  document.body.append(newMessege);
 };
 
 module.exports = {
