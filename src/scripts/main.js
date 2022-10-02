9c7f4acffab613afe534aa5fe442c2db60540e7e
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-02 17:21:59 +0300

    functions for promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const resolver = (complete) => {
+    element.addEventListener(eventName, () => {
+      const message = `It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`;
+
+      complete(message);
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
-// write your code here
+  const generalBox = document.querySelector('.messages');
+  const messageBox = `
+    <div class="message">
+      ${message}
+    </div>
+    `;
+
+  generalBox.insertAdjacentHTML('beforeend', messageBox);
 };
 
 module.exports = {
