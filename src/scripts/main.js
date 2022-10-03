6e8ac83eb6cbb9b27d01bf6295c3c297a3ec9165
Boris Kerekesha <kerekesha90@ukr.net>
2022-10-03 15:41:51 +0300

    promises_function_with one message for one event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,27 @@
 function waitFor(element, eventName) {
   const result = new Promise((resolve, reject) => {
     element.addEventListener(eventName, (evt) => {
-        resolve(`
+      evt.preventDefault();
+      resolve(`
         It was ${eventName} on the element:
         ${element.nodeName}, id: ${element.id}
-        `);
+      `);
     });
   });
 
+  result.then(() => {
+    document.getElementsByClassName('message')[0].remove();
+  });
+
   return result;
-}
+};
 
 const printMessage = (message) => {
   document.body.insertAdjacentHTML('afterbegin', `
-  <div class="message">
-    ${message}
-  </div>
-`);
+    <div class="message">
+      ${message}
+    </div>
+  `);
 };
 
 module.exports = {
