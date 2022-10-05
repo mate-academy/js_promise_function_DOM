ac53a8318859b9f40f4a60060e212d638bf40bb5
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-10-05 17:31:06 +0300

    Promise function DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName},
+      id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageElement = document.createElement('div');
+
+  messageElement.className = 'message';
+  messageElement.textContent = message;
+
+  document.body.append(messageElement);
 };
 
 module.exports = {
