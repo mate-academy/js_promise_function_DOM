258388cfe1230c7734a55c131fc58ba69110430a
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-28 00:23:59 +0300

    Implement promise function app

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,44 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName},
+        id: ${element.id}`
+      );
+    });
+  });
 }
 
-const printMessage = (message) => {
-// write your code here
-};
+const printMessage = createPush();
+
+function createPush() {
+  let counter = 0;
+
+  return function(message) {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.innerText = message;
+    document.body.append(div);
+
+    if ([...document.querySelectorAll('div')].length === 1) {
+      counter = 0;
+    }
+
+    if (!counter) {
+      counter++;
+    } else {
+      div.style.top = `
+      ${(30 * counter)
+        + div.getBoundingClientRect().height
+        * (counter)}px
+    `;
+      counter++;
+    }
+  };
+}
 
 module.exports = {
   waitFor,
