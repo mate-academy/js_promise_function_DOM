2e5eb44c5c1f0fdde8bb58eba6829c5cc619c68b
Zykhor Andrii <zuxoran@gmail.com>
2022-10-30 17:13:59 +0200

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,25 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      const result = `It was ${eventName} on the element: ${
+        element.nodeName}, id: ${element.id}.`;
+
+      resolve(result);
+    });
+});
 }
 
 const printMessage = (message) => {
 // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+
+  div.innerText = message;
+
+  document.body.append(div);
 };
 
 module.exports = {
