0e32030dfe58c198e58d13cd3022748867145744
Oleksandr Usov <usov.sasha.93@gmail.com>
2022-11-15 14:16:16 +0200

    Added a task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const div = document.createElement('div');
+
+div.classList.add('message');
+
 function waitFor(element, eventName) {
-// write your code here
-}
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(`
+        It was ${eventName} on the element: ${element.nodeName}, 
+        id: ${element.id}.
+      `);
+    });
+  });
+};
 
 const printMessage = (message) => {
-// write your code here
+  document.forms[0].insertAdjacentHTML('beforeend', `
+    <div class='message'>${message}</div>
+  `);
 };
 
 module.exports = {
