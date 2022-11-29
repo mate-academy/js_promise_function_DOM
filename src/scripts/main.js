9d4d735ae5daa1cf33b5daff143b48f12eb003e3
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-11-29 23:41:46 +0200

    Promise function has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const messagesContainer = document.createElement('div');
+
+messagesContainer.classList.add('messages-container');
+document.body.appendChild(messagesContainer);
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(`
+        It was ${eventName}
+        on the element: ${element.nodeName},
+        id: ${element.id}
+      `);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  messagesContainer.insertAdjacentHTML('beforeend', `
+    <div class="message">${message}</div>
+  `);
 };
 
 module.exports = {
