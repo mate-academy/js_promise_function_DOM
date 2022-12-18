caa8abe457bc35cb3bf45cdaf98cf6eb6205af2e
Yurii Teslia <teslya.yuriy@gmail.com>
2022-12-18 06:14:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
-// write your code here
+  // write your code here
+  const myPromise = new Promise((resolve) => {
+    element.addEventListener(eventName, (e) => {
+      if (e.target === element) {
+        resolve(`It was ${eventName} on the element: 
+              ${element.nodeName}, id: ${element.id}.`);
+      }
+    });
+  });
+
+  return myPromise;
 }
 
 const printMessage = (message) => {
-// write your code here
+  // write your code here
+  body.insertAdjacentHTML('beforeend', `<div class="message">${message}</div>`);
 };
 
 module.exports = {
