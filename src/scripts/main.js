0977ab4dcbb260c89ff74e2edf34cc887fe2d674
anton-sosonskyi <anton.sosonskyi@gmail.com>
2022-12-18 12:33:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,22 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      const target = e.target;
+      const elementInfo = `It was ${eventName} on the `
+      + `element: ${target.nodeName}, `
+      + `id: ${target.id}`;
+
+      resolve(elementInfo);
+    });
+  });
 }
 
 const printMessage = (message) => {
 // write your code here
+  document.body.insertAdjacentHTML('afterbegin',
+    `<div class="message">${message}</div>`);
 };
 
 module.exports = {
