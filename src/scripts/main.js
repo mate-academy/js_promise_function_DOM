759ed3a2ebab90c392179c79935ac99544566aab
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-12-25 17:25:46 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,8 @@ function waitFor(element, eventName) {
   return new Promise(resolve => {
     element.addEventListener(eventName, () => {
       const id = element.getAttribute('id');
-      const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${id}.`
+      const message = `It was ${eventName} on the element: ${element.nodeName}, 
+        id: ${id}.`;
 
       resolve(message);
     });
