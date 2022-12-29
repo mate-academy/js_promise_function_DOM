158e9eabb0ec1d00eb17bd4fcae2f27d229f5b90
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2022-12-29 22:44:18 +0200

    Promise function DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
-// write your code here
+  const waitPromise = new Promise((resolve) => {
+    element.addEventListener(eventName, (e) => {
+      if (e.target === element) {
+        resolve(`It was ${eventName} on the element:
+          ${element.nodeName}, id: ${element.id}.
+        `);
+      }
+    });
+  });
+
+  return waitPromise;
 }
 
 const printMessage = (message) => {
-// write your code here
+  body.insertAdjacentHTML('beforeend', `
+    <div class="message">${message}</div>
+  `);
 };
 
 module.exports = {
