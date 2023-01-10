1b498c00829bcd430422cb28db57e75dd2cba26a
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-01-10 16:29:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,23 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise(resolve => {
+    element.addEventListener(`${eventName}`, (e) => {
+      const message = `It was ${eventName} on the element:
+        ${e.target.nodeName}, id: ${e.target.id}.`;
+
+      resolve(message);
+    });
+  });
 }
 
 const printMessage = (message) => {
 // write your code here
+  const form = document.querySelector('form');
+
+  form.insertAdjacentHTML('beforeend', `
+    <div class="message" style="margin-top: 5px">${message}</div>
+  `);
 };
 
 module.exports = {
