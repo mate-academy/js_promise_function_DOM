c1b426c2f07f665d4f95f39f62365db741261274
Sergey Vorobey <sergey88nik@gmail.com>
2023-01-12 20:29:52 +0200

    Promise function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,33 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    document.addEventListener(`${eventName}`, (evt) => {
+      const elementToClick = evt.target;
+
+      if (element === elementToClick) {
+        resolve(`
+          It was ${eventName} on the element:
+          ${elementToClick.nodeName}, id: ${elementToClick.id}.
+        `);
+      }
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  createElement(message);
 };
 
 module.exports = {
   waitFor,
   printMessage,
 };
+
+function createElement(message) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${message};
+    </div>
+  `);
+}
