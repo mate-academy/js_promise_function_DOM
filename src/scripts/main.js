13df752a24cf408b246436a09c1025ba93bc598c
Taras Baran <baranprivat22@gmail.com>
2023-02-01 22:35:36 +0200

    added space

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 function waitFor(element, eventName) {
   return new Promise(resolve => {
     element.addEventListener(`${eventName}`, e => {
-      resolve(`It was ${eventName}`
-        + `on the element: ${element.nodeName},`
+      resolve(`It was ${eventName} `
+        + `on the element: ${element.nodeName}, `
         + `id: ${element.id}.`
       );
     });
