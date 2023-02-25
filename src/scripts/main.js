477583f6ac43d16b45258753deb89f7ff9476250
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-25 13:45:04 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const result = new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+       ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
+
+  return result;
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML(`beforeend`, `
+    <div class="message">${message}</div>
+  `);
 };
 
 module.exports = {
