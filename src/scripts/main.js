b81730c20e7c42a0e430ac5e479b794dace5b71f
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-05 23:54:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,31 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    function eventHandler(e) {
+      element.removeEventListener(eventName, eventHandler);
+
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}`
+        + `, id: ${element.id}`
+      );
+    }
+    element.addEventListener(eventName, eventHandler);
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+
+  messageDiv.textContent = message;
+  document.body.appendChild(messageDiv);
 };
+
+const myButton = document.getElementById('my-button');
+
+waitFor(myButton, 'click').then(message => printMessage(message));
 
 module.exports = {
   waitFor,
