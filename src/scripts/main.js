34d7dd0bb3ce413d412b07c71256ba8470f275a2
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-06 00:09:51 +0200

    Edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,30 +2,24 @@
 
 function waitFor(element, eventName) {
   return new Promise(resolve => {
-    function eventHandler(e) {
-      element.removeEventListener(eventName, eventHandler);
-
+    element.addEventListener(eventName, () => {
       resolve(
         `It was ${eventName} on the element: ${element.nodeName}`
         + `, id: ${element.id}`
       );
-    }
-    element.addEventListener(eventName, eventHandler);
+    });
   });
 }
 
+const messageDiv = document.createElement('div');
+const form = document.querySelector('form');
+
+form.append(messageDiv);
+
 const printMessage = (message) => {
-  const messageDiv = document.createElement('div');
-
   messageDiv.classList.add('message');
-
   messageDiv.textContent = message;
-  document.body.appendChild(messageDiv);
 };
-
-const myButton = document.getElementById('my-button');
-
-waitFor(myButton, 'click').then(message => printMessage(message));
 
 module.exports = {
   waitFor,
