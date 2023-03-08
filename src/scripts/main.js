af9fea60d4f88a1e8480bfa60856463ff3178d9a
Andrew Grabynsky <hrabynsky@gmail.com>
2023-03-08 22:01:50 +0200

    [SOLUTION] Made promise and message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
+const block = document.createElement('div');
+
+document.body.append(block);
+
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, () => {
+      resolve(`
+      It was ${eventName} on the element: ${element.nodeName} id: ${element.id}.
+      `);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  block.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${message}
+    </div>
+  `);
 };
 
 module.exports = {
