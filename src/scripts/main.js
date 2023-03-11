425ce5f368a276ced0e7c875bdd0ced955bd05d7
Oleksandr <98667890az@gmail.com>
2023-03-11 17:04:53 +0200

    Promise function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('click', (e) => {
+      resolve(`It was ${eventName}
+      on the element: ${element.nodeName},
+      id: ${element.id}`);
+    });
+  }).then().catch();
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML('beforeend',
+    `<div class="message"> ${message} </div>`
+  );
 };
 
 module.exports = {
