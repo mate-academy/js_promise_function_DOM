e56e50d534742eb82da21ce84de0d56526c97aea
Serhii Melnychenko <8melya8@gmail.com>
2023-03-28 17:23:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(`${eventName}`, () => {
+      resolve(
+        `It was ${eventName}`
+      + ` on the element: ${element.nodeName},`
+      + ` id: ${element.id}.`
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">
+      ${message}
+    </div>
+  `);
 };
 
 module.exports = {
