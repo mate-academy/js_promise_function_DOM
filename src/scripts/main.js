d17904a2d45fe3db0d5e81662ae3898d3a9d1de9
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-30 16:43:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,17 +11,16 @@ function waitFor(element, eventName) {
   });
 }
 
-const div = document.querySelector('.message');
-
 const printMessage = (message) => {
+  let div = document.querySelector('.message');
   if (div) {
     div.textContent = message;
   } else {
-    const divMessage = document.createElement('div');
+    div = document.createElement('div');
 
-    divMessage.classList.add('message');
-    divMessage.textContent = message;
-    document.body.appendChild(divMessage);
+    div.classList.add('message');
+    div.textContent = message;
+    document.body.appendChild(div);
   };
 };
 
