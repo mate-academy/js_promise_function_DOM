d65ed05a0bfa0e3bdc393252d2bf635667735495
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-05 12:29:49 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      /* eslint-disable max-len */
+      const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+
+      resolve(
+        printMessage(message)
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const notificationMessage = document.createElement('div');
+
+  notificationMessage.classList.add('message');
+  notificationMessage.innerText = message;
+  document.body.appendChild(notificationMessage);
 };
 
 module.exports = {
