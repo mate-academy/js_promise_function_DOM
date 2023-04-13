f2afcde78bce4fda901ef0a78170085e00a35d0d
Aleksey-Aleksey <l0734062646@gmail.com>
2023-04-13 19:27:30 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName},
+      id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const body = document.body;
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  body.append(div);
 };
 
 module.exports = {
