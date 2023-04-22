b750d69aee088974c866a143ec3de74b2db70f0d
адмін <admin@Mac-Max.local>
2023-04-22 12:52:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,33 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    const handler = () => {
+      resolve(
+        `It was ${eventName}
+        on the element: ${element.nodeName},
+        id: ${element.id}.`
+      );
+    };
+
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const notificationId = '1';
+  let notificationElement = document.getElementById(notificationId);
+
+  if (notificationElement) {
+    notificationElement.remove();
+  }
+
+  notificationElement = document.createElement('div');
+  notificationElement.id = notificationId;
+  notificationElement.classList.add('message');
+  notificationElement.innerText = message;
+
+  document.body.appendChild(notificationElement);
 };
 
 module.exports = {
