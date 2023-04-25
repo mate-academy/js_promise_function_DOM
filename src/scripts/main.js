584d71dea37d7f376b44eb91ea770343f6216415
Anna Grechana <grechanaanna@gmial.com>
2023-04-26 00:03:14 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName},
+       id: ${element.id}.`);
+    });
+  });
 }
 
+const div = document.createElement('div');
+const body = document.querySelector('body');
+
 const printMessage = (message) => {
-// write your code here
+  body.append(div);
+  div.classList = 'message';
+  div.textContent = message;
 };
 
 module.exports = {
