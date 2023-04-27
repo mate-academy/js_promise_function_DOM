35e2f3cde59ff7ad3577d0389eb0b0f1ca8d4f73
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-27 21:48:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
+/* eslint-disable max-len */
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+
+      resolve(message);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.innerText = message;
+  document.body.append(div);
 };
 
 module.exports = {
