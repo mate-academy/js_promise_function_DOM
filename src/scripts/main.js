2fd1cebb04adafbd253d7e43a89ffe25a36df74b
unknown <vorobetst777@gmail.com>
2023-05-03 00:19:53 +0300

    Ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,16 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => {
+    element.addEventListener(eventName, (e) => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+    })
+  }) 
 }
 
 const printMessage = (message) => {
-// write your code here
+  const newDiv = `<div class="message">${message}</div>`;
+  document.body.insertAdjacentHTML('beforeend', newDiv);
 };
 
 module.exports = {
