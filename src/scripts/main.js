a232c668a416847c4eecd5e14cf4357888404aae
Olena Shevchenko <106619964+Olena-Shevchenko@users.noreply.github.com>
2023-05-04 12:05:38 +0300

    Update src/scripts/main.js
    Co-authored-by: Oleksandr Polotniuk <96409563+polosanya@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,8 @@ function waitFor(element, eventName) {
   return new Promise((resolve, reject) => {
     element.addEventListener(eventName, () => {
       resolve(`
-      It was ${eventName} on the element: 
-      ${element.nodeName}, id: ${element.id}.
+        It was ${eventName} on the element: 
+        ${element.nodeName}, id: ${element.id}.
       `);
     });
   });
