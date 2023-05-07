5dc7163e07a5af7e703f6e96c6a64036243b1229
metin <metinbicaksiz@gmail.com>
2023-05-07 13:33:21 +0300

    functions trial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,34 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise(resolve => {
+    // eslint-disable-next-line no-shadow
+    function handler(event) {
+      element.removeEventListener(eventName, handler);
+
+      resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
+    }
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
 // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 module.exports = {
   waitFor,
   printMessage,
 };
+
+const myButton = document.getElementById('button');
+
+waitFor(myButton, 'click').then(message => {
+  printMessage(message);
+});
