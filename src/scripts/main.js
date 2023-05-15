63c61c67b92625101c7d65dd74e6adb6a0898abe
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-15 15:41:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const resolver = (resolve) => {
+    element.addEventListener(eventName, () => {
+      const message
+        = `It was ${eventName}`
+        + ` on the element: ${element.nodeName}, id: ${element.id}.`
+        + ` (For example: It was click on the element: INPUT, id: login.`;
+
+      printMessage(message);
+      resolve();
+    });
+  };
+
+  return new Promise(resolver);
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageContainer = document.createElement('div');
+
+  messageContainer.className = 'message';
+  messageContainer.innerText = message;
+  document.body.append(message);
 };
 
 module.exports = {
