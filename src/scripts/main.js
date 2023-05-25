8c46279b7a475a57569f44fc9bef8d9d67d2bc88
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-25 19:04:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,22 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+      ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  // write your code here
+  const body = document.querySelector('form');
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  body.prepend(div);
 };
 
 module.exports = {
