b49ae669dfb05b29f0d03dd895eb5be32513ae52
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-27 00:03:21 +0300

    js_promise-function-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, e => {
+      resolve(
+        `
+          It was ${eventName} on the element:
+          ${element.tagName}, id: ${element.id}.
+        `
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageElement = document.createElement('div');
+
+  messageElement.classList.add('message');
+  messageElement.innerHTML = `<p>${message}</p>`;
+  document.body.append(messageElement);
 };
 
 module.exports = {
