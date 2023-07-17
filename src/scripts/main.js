8bb0518ce8542e99e8f975b67f8166e5c20b3f81
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-17 16:52:12 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  const promise1 = (resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      let result = `It was ${eventName} on the element: `;
+
+      result += `${element.nodeName}, id: ${element.id}.`;
+      resolve(result);
+    });
+  };
+
+  return new Promise(promise1);
 }
 
 const printMessage = (message) => {
-// write your code here
+  const div = document.getElementById('div');
+
+  div.innerText = message;
 };
 
 module.exports = {
