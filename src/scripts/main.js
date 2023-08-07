0cc7541a20375cee1575e5142d95cc800d2eabe4
Szymon Pachucki <szymonpachucki010@gmail.com>
2023-08-07 17:49:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise(resolve => addEventListener(eventName, () =>
+    resolve('It was ' + eventName + ' on the element: '
+  + element.nodeName + ', id: ' + 'element.id.'), {
+    once: false,
+  }));
 }
 
+let messageDisplayed = false;
+
 const printMessage = (message) => {
-// write your code here
+  if (!messageDisplayed) {
+    const messageDiv = document.createElement('div');
+
+    messageDiv.classList.add('message');
+    document.body.appendChild(messageDiv);
+    messageDiv.textContent = message;
+
+    messageDisplayed = true;
+  }
 };
 
 module.exports = {
