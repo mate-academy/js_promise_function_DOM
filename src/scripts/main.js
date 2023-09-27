9a0acf548db4097b9f3d863fce949fa0cea3526b
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-09-27 23:28:02 +0200

    js_promise_function_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    const eventListener = () => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName},
+      id: ${element.id}.`);
+      element.removeEventListener(eventName, eventListener);
+    };
+
+    element.addEventListener(eventName, eventListener);
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = message;
+  document.body.appendChild(messageDiv);
 };
 
 module.exports = {
