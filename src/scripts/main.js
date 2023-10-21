e91e8643bd485be14ef16d10dadc8319b68b60fb
Volodymyr <psn22782@gmail.com>
2023-10-21 22:50:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      // eslint-disable-next-line
+      resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-// write your code here
+  document.body.insertAdjacentHTML('beforeend',
+    `
+      <div class="message">
+        ${message}
+      </div>
+    `);
 };
 
 module.exports = {
