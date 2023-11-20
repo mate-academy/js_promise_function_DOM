76f298cf87ae9ae3226e8f2b0a092bdc2b04daf1
Misha Hrynko <misha@mate.academy>
2023-11-20 18:41:54 +0200

    fix depth

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-// write your code here
+  // write your code here
 }
 
 const printMessage = (message) => {
-// write your code here
+  // write your code here
 };
 
-module.exports = {
-  waitFor,
-  printMessage,
-};
+const loginField = document.getElementById('login');
+const passwordField = document.getElementById('password');
+const button = document.getElementById('submit');
+
+waitFor(loginField, 'click').then(printMessage);
+waitFor(passwordField, 'click').then(printMessage);
+waitFor(button, 'click').then(printMessage);
+
+waitFor(loginField, 'input').then(printMessage);
+waitFor(passwordField, 'input').then(printMessage);
+
+waitFor(loginField, 'blur').then(printMessage);
+waitFor(passwordField, 'blur').then(printMessage);
+waitFor(button, 'blur').then(printMessage);
