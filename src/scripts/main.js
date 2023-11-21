a899f1a21b7d8201560fba2396225f2861b9e472
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-21 18:37:11 +0100

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,23 @@
 
 function waitFor(element, eventName) {
 // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: ${
+        element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
+
+// it will propably work, but i cant use npm anyway
 
 const printMessage = (message) => {
 // write your code here
+  const mydiv = document.createElement('div');
+
+  mydiv.classList.add('message');
+  mydiv.innerText = message;
+  document.body.appendChild(mydiv);
 };
 
 module.exports = {
