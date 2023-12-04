2501a99b9c77c177b56da51e64292d76b5ae8949
newqou <137334905+newqou@users.noreply.github.com>
2023-12-04 21:28:31 +0200

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,38 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  return new Promise((resolve) => {
+  const executor = (reslove) => {
     element.addEventListener(eventName, () => {
-      resolve(`It was ${eventName} on the element: ${element.nodeName},
-        id: ${element.id}.`);
+      reslove(
+        `It was ${eventName} on the element:
+         ${element.nodeName}, id: ${element.id}.`,
+      );
     });
-  });
+  };
+
+  return new Promise(executor);
 }
 
 const printMessage = (message) => {
-  const notification = document.createElement('div');
+  const box = document.createElement('div');
 
-  notification.classList = 'message';
+  box.className = 'message';
+  box.textContent = message;
 
-  notification.textContent = `${message}`;
-
-  return document.body.appendChild(notification);
+  document.body.appendChild(box);
 };
 
-module.exports = {
-  waitFor,
-  printMessage,
-};
+const loginField = document.getElementById('login');
+const passwordField = document.getElementById('password');
+const button = document.getElementById('submit');
+
+waitFor(loginField, 'click').then(printMessage);
+waitFor(passwordField, 'click').then(printMessage);
+waitFor(button, 'click').then(printMessage);
+
+waitFor(loginField, 'input').then(printMessage);
+waitFor(passwordField, 'input').then(printMessage);
+
+waitFor(loginField, 'blur').then(printMessage);
+waitFor(passwordField, 'blur').then(printMessage);
+waitFor(button, 'blur').then(printMessage);
