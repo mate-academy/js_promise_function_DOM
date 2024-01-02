2f7c13c989f021b8b0e6e17132c643b4cfbb2b65
Robert Szałapak <robsza1212@gmail.com>
2024-01-02 13:24:06 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  const divElement = document.getElementById(element);
+
+  return new Promise(function(resolve, reject) {
+    divElement.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element:
+       ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const formElement = document.querySelector('form');
+
+  formElement.className('message');
+  formElement.insertAdjacentHTML('afterbegin', `<div>${message}</div>`);
 };
 
 const loginField = document.getElementById('login');
