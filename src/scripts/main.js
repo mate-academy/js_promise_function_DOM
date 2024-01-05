6a2862bcbd439c31a2db31992130f6bd0aeed5f5
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-05 09:16:02 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName}`
+      + ` on the element: ${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  document.querySelector('form').insertAdjacentHTML('afterbegin', `
+  <div class="message">
+    ${message}
+  </div>
+`);
 };
 
 const loginField = document.getElementById('login');
@@ -15,10 +24,8 @@ const button = document.getElementById('submit');
 waitFor(loginField, 'click').then(printMessage);
 waitFor(passwordField, 'click').then(printMessage);
 waitFor(button, 'click').then(printMessage);
-
 waitFor(loginField, 'input').then(printMessage);
 waitFor(passwordField, 'input').then(printMessage);
-
 waitFor(loginField, 'blur').then(printMessage);
 waitFor(passwordField, 'blur').then(printMessage);
 waitFor(button, 'blur').then(printMessage);
