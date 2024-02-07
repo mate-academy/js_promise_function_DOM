cfefb77edecbc1f79994ed55b91bf3c31bdea01d
Dmytro Loboda <dmytro.loboda@gmail.com>
2024-02-07 17:24:41 +0800

    js_promise_function solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: `
+        + `${element.nodeName}, id: ${element.id}.`);
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+  messageDiv.textContent = message;
+  body.appendChild(messageDiv);
 };
 
 const loginField = document.getElementById('login');
