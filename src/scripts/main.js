d6405ddcd11d44e1ad4fa617d6be63f0002a0ce8
Ярина Пугач <puhachyaryna@gmail.com>
2024-03-11 15:00:24 +0100

    problems with  test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`
+        It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.
+      `);
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const messageContainer = document.createElement('div');
+
+  messageContainer.className = 'message';
+  messageContainer.innerText = message;
+  document.body.append(messageContainer);
 };
 
 const loginField = document.getElementById('login');
