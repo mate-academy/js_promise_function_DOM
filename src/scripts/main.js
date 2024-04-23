ebf9bb1f417a407fe562292a6a154bd846bd74b4
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-04-23 15:38:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,36 +2,20 @@
 
 function waitFor(element, eventName) {
   return new Promise((resolve, reject) => {
-    element.addEventListener(eventName, () => {
-      resolve(
-        `It was ${eventName} ` +
-          `on the element: ${element.nodeName}, ` +
-          `id: ${element.id}.`,
-      );
+    element.addEventListener(`${eventName}`, () => {
+      resolve(`It was ${eventName} on the element:
+        ${element.nodeName}, id: ${element.id}.`);
     });
   });
 }
 
-
 const printMessage = (message) => {
-  const msg = document.createElement('div');
-
-  msg.classList.add('message');
-  msg.innerHTML = message;
-  document.getElementById('messages').appendChild(msg);
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="message">${message}</div>
+  `);
 };
 
-const loginField = document.getElementById('login');
-const passwordField = document.getElementById('password');
-const button = document.getElementById('submit');
-
-waitFor(loginField, 'click').then(printMessage);
-waitFor(passwordField, 'click').then(printMessage);
-waitFor(button, 'click').then(printMessage);
-
-waitFor(loginField, 'input').then(printMessage);
-waitFor(passwordField, 'input').then(printMessage);
-
-waitFor(loginField, 'blur').then(printMessage);
-waitFor(passwordField, 'blur').then(printMessage);
-waitFor(button, 'blur').then(printMessage);
+module.exports = {
+  waitFor,
+  printMessage,
+};
