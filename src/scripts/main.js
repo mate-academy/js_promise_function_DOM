eefb4990c53b0377ece0e12f53568c7fa538bf92
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-04-23 15:23:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} ` +
+          `on the element: ${element.nodeName}, ` +
+          `id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const msg = document.createElement('div');
+
+  msg.classList.add('message');
+  msg.innerHTML = message;
+  document.getElementById('messages').appendChild(msg);
 };
 
 const loginField = document.getElementById('login');
