1c0518ed8df18b4ca700b8a04b41db95e6af00da
Damian <damian.adamczewski92@gmail.com>
2024-05-06 16:56:41 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+
+  document.forms[0].append(div);
 };
 
 const loginField = document.getElementById('login');
