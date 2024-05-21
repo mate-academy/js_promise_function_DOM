a75147fc82e6afd3aa700e64caf538befb240b00
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-05-21 10:26:29 +0300

    promise function DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const messageElement = document.createElement('div');
+
+  messageElement.classList.add('message');
+  messageElement.textContent = message;
+  document.body.appendChild(messageElement);
+
+  setTimeout(() => {
+    messageElement.remove();
+  }, 2000);
 };
 
 const loginField = document.getElementById('login');
