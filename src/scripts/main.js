b86927df479a24270c0e98e79423158cc9857054
Rostuk Dudchak <rostikdudchak15@gmail.com>
2024-06-14 15:28:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.innerHTML = message;
+  div.style.position = 'fixed';
+  div.style.top = '0';
+  div.style.right = '0';
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
