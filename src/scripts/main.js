4ad8e61a6cf3c19981e05771c8345f9bbabaaa6a
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-21 20:32:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(
+      eventName,
+      () => {
+        const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+
+        resolve(message);
+      },
+      { once: true },
+    );
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const notification = document.createElement('div');
+
+  notification.className = 'message';
+  notification.textContent = message;
+  document.body.appendChild(notification);
 };
 
 const loginField = document.getElementById('login');
