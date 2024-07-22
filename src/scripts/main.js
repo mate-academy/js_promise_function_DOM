bb436b38eb1f84851af74da9b86a125006f122c4
luter <newgglutt@gmail.com>
2024-07-22 17:04:10 +0300

    s

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+    }, { once: true });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const notification = document.createElement('div');
+  notification.className = 'message';
+  notification.textContent = message;
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
 };
 
 const loginField = document.getElementById('login');
