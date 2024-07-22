ea1d7c1e77dc266b7c2a18ab1aa157f4e3b35380
luter <newgglutt@gmail.com>
2024-07-22 17:05:37 +0300

    s1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,21 @@
 
 function waitFor(element, eventName) {
   return new Promise((resolve) => {
-    element.addEventListener(eventName, () => {
-      resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
-    }, { once: true });
+    element.addEventListener(
+      eventName,
+      () => {
+        resolve(
+          `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+        );
+      },
+      { once: true },
+    );
   });
 }
 
 const printMessage = (message) => {
   const notification = document.createElement('div');
+
   notification.className = 'message';
   notification.textContent = message;
   document.body.appendChild(notification);
