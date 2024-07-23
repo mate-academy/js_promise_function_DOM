bd9cc97fea9d003422108ba9c0ea68b198de104e
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-07-23 18:18:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    const eventHandler = () => {
+      const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+
+      element.removeEventListener(eventName, eventHandler);
+      resolve(message);
+    };
+
+    element.addEventListener(eventName, eventHandler);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
