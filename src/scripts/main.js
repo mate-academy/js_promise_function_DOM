f5791c2826bb36d3610758d8678bc20c9025a629
svyatoslav <svyatoslavik9@gmail.com>
2024-08-01 11:40:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
   // write your code here
+  /* eslint-disable promise/param-names */
+  return new Promise((resolve, _) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('message');
+  notification.textContent = message;
+
+  body.appendChild(notification);
 };
 
 const loginField = document.getElementById('login');
