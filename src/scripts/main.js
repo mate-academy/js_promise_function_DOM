1abb8d241f6bf30855c441521bcd300fa98dc859
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-08-11 03:25:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  const promise = new Promise(function (resolve) {
+    element.addEventListener(eventName, (e) => {
+      if (e.target === element) {
+        resolve(
+          `It was ${eventName} on the element:
+${element.nodeName}, id: ${element.id}.`,
+        );
+      }
+    });
+  });
+
+  return promise;
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+  // const body = document.body;
+
+  div.textContent = message;
+  document.body.appendChild(div);
+  div.className = 'message';
 };
 
 const loginField = document.getElementById('login');
