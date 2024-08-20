3c0b4e9ca1919cb9a223cc4f392f503db3eb31ca
Vlados <vnegoluk@gmail.com>
2024-08-20 15:36:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,24 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName.toUpperCase()}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  div.innerHTML = message;
+
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
