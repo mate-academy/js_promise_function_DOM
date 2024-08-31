e771169fbbe25c0851fa8a64097e85e97a1f9bb0
iryna lysko <goiralysko@gmail.com>
2024-08-31 13:26:08 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    const resolver = () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+
+      element.removeEventListener(eventName, resolver);
+    };
+
+    element.addEventListener(eventName, resolver);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const body = document.querySelector('body');
+
+  body.insertAdjacentHTML('beforeend', `<div class="message">${message}</div>`);
 };
 
 const loginField = document.getElementById('login');
