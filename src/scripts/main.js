37265a4c5ffdfcae05dacb90573c0193cb9b16c9
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-22 22:25:11 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(`${eventName}`, () => {
+      resolve(() => {
+        return `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+      });
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const mess = document.createElement('div');
+
+  mess.classList.add('message');
+  mess.textContent = message();
+
+  document.body.appendChild(mess);
 };
 
 const loginField = document.getElementById('login');
