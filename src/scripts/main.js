79da3797bfd96d0dbae9ba7f43f33a007a395b9f
Iryna <irakogut23190@gmail.com>
2025-01-13 13:13:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(`${eventName}`, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, ${element.id}`,
+      );
+    });
+  });
 }
 
-const printMessage = (message) => {
-  // write your code here
+const printMessage = async (message) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = await message;
+
+  document.body.append(div);
 };
 
 const loginField = document.getElementById('login');
