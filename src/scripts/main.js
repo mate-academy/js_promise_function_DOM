6aa3df70bbfc54b3a3b3447deea566e72d42c48b
iteamfox <avlisunenko@gmail.com>
2025-02-19 20:52:22 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,37 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve, reject) => {
+    if (!(element instanceof HTMLElement)) {
+      reject(new Error('First argument must be a valid DOM element'));
+
+      return;
+    }
+
+    if (typeof eventName !== 'string') {
+      reject(new Error('Second argument must be a valid event name (string)'));
+
+      return;
+    }
+
+    const handler = () => {
+      element.removeEventListener(eventName, handler);
+
+      resolve(`
+        It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+    };
+
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const messageDiv = document.createElement('div');
+
+  messageDiv.classList.add('message');
+  messageDiv.textContent = message;
+  document.body.appendChild(messageDiv);
 };
 
 const loginField = document.getElementById('login');
