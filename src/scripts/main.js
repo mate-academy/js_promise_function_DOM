721850d4b9f9fdcf2500d303ebcd80e5d786db15
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-02-22 14:07:58 -0600

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const messageBody = document.createElement('div');
+
+  messageBody.classList.add('message');
+  messageBody.textContent = message;
+  document.body.append(messageBody);
 };
 
 const loginField = document.getElementById('login');
