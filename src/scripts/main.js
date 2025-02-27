918331847d7107b3cafdc88bfdc8aae9029f0371
vkrasnovyd <v.krasnovyd@gmail.com>
2025-02-27 11:14:22 +0100

    Sulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const body = document.querySelector('body');
+  const messageElement = document.createElement('div');
+
+  messageElement.classList.add('message');
+  messageElement.textContent = message;
+
+  body.appendChild(messageElement);
 };
 
 const loginField = document.getElementById('login');
