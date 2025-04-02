cb37c2f8c0cf8a1898f491347bf0cf37eddc4208
my-git-username <118355685+my-git-username@users.noreply.github.com>
2025-04-02 15:55:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    function eventHandler() {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+      element.removeEventListener(eventName, eventHandler);
+    }
+    element.addEventListener(eventName, eventHandler);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const notification = document.createElement('div');
+
+  notification.textContent = message;
+  notification.className = 'message';
+
+  document.body.appendChild(notification);
 };
 
 const loginField = document.getElementById('login');
