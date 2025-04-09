9201313f9f67726f5270fc3092bf850b88a1a930
sirserg <sirserg05@gmail.com>
2025-04-09 13:17:19 +0300

    feat: add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () =>
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      ),
+    );
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const container = document.createElement('div');
+
+  container.classList.add('message');
+  container.textContent = message;
+  document.body.append(container);
 };
 
 const loginField = document.getElementById('login');
