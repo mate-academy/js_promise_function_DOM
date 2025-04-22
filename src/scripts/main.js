6286ae68626d1b9802cfc294d18880885e36f634
Kostya Oliinyk <aquila0905@gmail.com>
2025-04-22 21:32:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,27 @@
 'use strict';
 
-function waitFor(element, eventName) {
-  // write your code here
+export function waitFor(element, eventName) {
+  return new Promise((resolve) => {
+    const handler = () => {
+      element.removeEventListener(eventName, handler);
+
+      const nodeName = element.nodeName;
+      const id = element.id || 'no-id';
+
+      resolve(`It was ${eventName} on the element: ${nodeName}, id: ${id}.`);
+    };
+
+    element.addEventListener(eventName, handler);
+  });
 }
 
-const printMessage = (message) => {
-  // write your code here
-};
+export function printMessage(message) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+}
 
 const loginField = document.getElementById('login');
 const passwordField = document.getElementById('password');
