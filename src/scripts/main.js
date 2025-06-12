238eaef083e40fa6befcc319634d5900aa7d6a3a
dminikulin <dminikulin2003@gmail.com>
2025-06-12 11:51:32 +0300

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,29 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+
+  const existingDiv = document.querySelector('.message');
+
+  if (existingDiv) {
+    existingDiv.remove();
+  }
+
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
