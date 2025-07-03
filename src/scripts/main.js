54040478d045a42a7f26ffd282606221bd50982d
Bernavschi Natalia <natb@list.ru>
2025-07-03 13:56:42 +0300

    add task solution for js_promise_function_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,43 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    const handler = () => {
+      element.removeEventListener(eventName, handler);
+
+      const message = `
+        It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.
+      `;
+
+      resolve(message);
+    };
+
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
 const passwordField = document.getElementById('password');
 const button = document.getElementById('submit');
 
+// click
 waitFor(loginField, 'click').then(printMessage);
 waitFor(passwordField, 'click').then(printMessage);
 waitFor(button, 'click').then(printMessage);
 
+// input
 waitFor(loginField, 'input').then(printMessage);
 waitFor(passwordField, 'input').then(printMessage);
 
+// blur
 waitFor(loginField, 'blur').then(printMessage);
 waitFor(passwordField, 'blur').then(printMessage);
 waitFor(button, 'blur').then(printMessage);
