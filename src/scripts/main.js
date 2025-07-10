5d6a4a5dd397e845f63aaae2d129d1f880fd68b9
Vanya Holovey <golovejvana9@gmail.com>
2025-07-10 14:41:51 +0300

    fixed resolve string

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,9 @@
 function waitFor(element, eventName) {
   return new Promise((resolve, reject) => {
     element.addEventListener(eventName, () => {
-      resolve(`It was ${eventName} on the element: ${element.nodeName},
-         id: ${element.id}.`);
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
     });
   });
 }
