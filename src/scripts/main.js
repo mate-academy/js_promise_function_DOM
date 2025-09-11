89a8bc67099041c19cdddb11453c59543301fc0e
annakalmazan <klavachichikova@gmail.com>
2025-09-11 15:11:18 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,46 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  if (
+    !element ||
+    !(element instanceof Element) ||
+    typeof eventName !== 'string'
+  ) {
+    throw new Error('Invalid arguments');
+  }
+
+  return new Promise((resolve) => {
+    element.addEventListener(
+      eventName,
+      () => {
+        resolve(
+          `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+        );
+      },
+      { once: true },
+    );
+  });
 }
 
-const printMessage = (message) => {
-  // write your code here
-};
+function printMessage(message) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
+}
 
 const loginField = document.getElementById('login');
 const passwordField = document.getElementById('password');
 const button = document.getElementById('submit');
 
 waitFor(loginField, 'click').then(printMessage);
-waitFor(passwordField, 'click').then(printMessage);
-waitFor(button, 'click').then(printMessage);
-
 waitFor(loginField, 'input').then(printMessage);
-waitFor(passwordField, 'input').then(printMessage);
-
 waitFor(loginField, 'blur').then(printMessage);
+
+waitFor(passwordField, 'click').then(printMessage);
+waitFor(passwordField, 'input').then(printMessage);
 waitFor(passwordField, 'blur').then(printMessage);
+
+waitFor(button, 'click').then(printMessage);
 waitFor(button, 'blur').then(printMessage);
