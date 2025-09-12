d3c24d307331d808eed1169442628d29fd743037
Alya <alinataranik03@gmail.com>
2025-09-12 20:55:44 +0300

    add task solution 12.09.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    function handler(ev) {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+      element.removeEventListener(eventName, handler);
+    }
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const d1v = document.createElement('div');
+
+  d1v.classList.add('message');
+  d1v.textContent = message;
+  document.body.appendChild(d1v);
 };
 
 const loginField = document.getElementById('login');
