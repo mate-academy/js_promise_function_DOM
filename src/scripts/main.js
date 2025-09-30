f0a6a55523f45a73f809e8ff7b49f7820bf253bd
S-Khraban <s.hraban@i.ua>
2025-09-30 09:38:30 +0300

    _add export for function_

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,3 +36,6 @@ waitFor(passwordField, 'input').then(printMessage);
 waitFor(loginField, 'blur').then(printMessage);
 waitFor(passwordField, 'blur').then(printMessage);
 waitFor(button, 'blur').then(printMessage);
+
+window.waitFor = waitFor;
+window.printMessage = printMessage;
