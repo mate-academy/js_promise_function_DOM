74b3975bbcbf96189609842835e092ae9c02db41
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-10-14 18:25:46 -0600

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,26 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve) => {
+    const handler = () => {
+      element.removeEventListener(eventName, handler);
+
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    };
+
+    element.addEventListener(eventName, handler);
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const newMessage = document.createElement('div');
+
+  newMessage.classList.add('message');
+  newMessage.textContent = message;
+  document.body.appendChild(newMessage);
 };
 
 const loginField = document.getElementById('login');
