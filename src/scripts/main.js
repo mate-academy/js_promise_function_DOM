3735792d8f8f5c969a2e40b95529736bf9da0761
Svitlana Chepa <svetachepa2320@gmail.com>
2025-10-20 09:09:00 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,24 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve) => {
+    const handler = () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    };
+
+    element.addEventListener(eventName, handler, { once: true });
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
