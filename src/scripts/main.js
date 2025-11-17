95d1e018e6e9a63ad68a9c75b64a604d77c15d25
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-17 11:48:03 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 function waitFor(element, eventName) {
   // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
   // write your code here
+  const div = document.createElement('div');
+
+  div.innerText = message;
+  div.classList.add('message');
+  body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
