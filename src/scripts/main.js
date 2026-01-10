0db7c20e35078a0d7beb2cd9bf26fba2a41c2d52
Horoshko Vitaliy <horoshko29@gmail.com>
2026-01-10 23:21:39 +0200

    #3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,7 @@
 function waitFor(element, eventName) {
   return new Promise((resolve) => {
     function handler() {
-      resolve(
-        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
-      );
+      resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
       element.removeEventListener(eventName, handler);
     }
     element.addEventListener(eventName, handler);
