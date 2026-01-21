7e8f5a49570c23a2d0888a2bd63541cf588ca25f
vladiksanl <vladislav.liashenko1@gmail.com>
2026-01-21 17:48:37 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  const promiseOfFunction = new Promise((resolve) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
+
+  return promiseOfFunction;
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+
+  div.textContent = message;
+
+  document.body.append(div);
 };
 
 const loginField = document.getElementById('login');
