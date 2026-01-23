c5e2e5ef77de99541d1c1980deac50aa6d6e6c5b
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-23 12:06:17 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,25 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  const promise = new Promise((resolve, reject) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
+
+  return promise;
 }
 
-const printMessage = (message) => {
-  // write your code here
-};
+function printMessage(message) {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+
+  document.body.append(div);
+}
 
 const loginField = document.getElementById('login');
 const passwordField = document.getElementById('password');
