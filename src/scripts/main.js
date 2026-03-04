1295720d71020cac00841f9396108db02bd1211b
Serhii Shimko <shimkosergij2010@gmail.com>
2026-03-04 10:30:04 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+  const body = document.querySelector('body');
+
+  div.textContent = message;
+  div.classList.add('message');
+
+  body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
