4a637c865727abe3ce0cd976a78d800df16cb126
Oksana Rubel <okhymych480@gmail.com>
2026-03-16 14:17:21 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,30 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    function onClick() {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+      element.removeEventListener(eventName, onClick);
+    }
+
+    element.addEventListener(eventName, onClick);
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  body.append(div);
 };
 
 const loginField = document.getElementById('login');
 const passwordField = document.getElementById('password');
 const button = document.getElementById('submit');
+const body = document.querySelector('body');
 
 waitFor(loginField, 'click').then(printMessage);
 waitFor(passwordField, 'click').then(printMessage);
