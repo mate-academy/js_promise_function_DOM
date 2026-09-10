20674da82b9750173efb68ab68330ee77e9101b3
Pavlo-Kurchak <plauss@icloud.com>
2026-09-10 15:39:36 +0300

    done tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    element.addEventListener(eventName, () => {
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const divCon = document.createElement('div');
+
+  divCon.classList.add('message');
+  divCon.textContent = message;
+
+  document.body.appendChild(divCon);
 };
 
 const loginField = document.getElementById('login');
